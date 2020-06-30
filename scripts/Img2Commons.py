c74f25c66852f0cf1a4455181f61b0f8e28ce351
Rick Companje <rick@companje.nl>
2020-06-30 21:44:47 +0200

    --rows 1 500

--- scripts/Img2Commons.py
+++ scripts/Img2Commons.py
@@ -6,18 +6,23 @@ from CommonsUpload import upload
 import pandas
 from liquid import Liquid
 
+from datetime import datetime
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Uploads images in batch to Wikimedia Commons')
   parser.add_argument('--csv',help="input csv file", required=True)
   parser.add_argument('--metadata',help="metadata template", required=True)
   parser.add_argument('--local-image',help="local image template", required=True)
   parser.add_argument('--remote-image',help="remote image template", required=True)
-  parser.add_argument('--login',nargs=2)
+  parser.add_argument('--login',nargs=2,help="username password")
+  parser.add_argument('--rows',nargs=2,help="start-row end-row")
   # response = login(sys.argv[1], sys.argv[2], "session")
 
   parser.add_argument('action', choices=['test', 'upload'])
   parser.add_argument('--verbose', '-v', action='count', default=0)
   args = parser.parse_args()
+
+print(datetime.now().strftime("%H:%M:%S"))
 
 if args.login:
   print("Login...")
@@ -40,9 +45,16 @@ liq_meta = Liquid(args.metadata, liquid_from_file=True)
 liq_local = Liquid(args.local_image, liquid_from_file=True) 
 liq_remote = Liquid(args.remote_image, liquid_from_file=True) 
 
+#select rows
+if args.rows:
+  begin=int(args.rows[0])
+  end=int(args.rows[1])
+else:
+  begin=0
+  end=len(csv_dict)
+
 # for each record
-for i in range(0,len(csv_dict)):
-  
+for i in range(begin,end):
   try:
     row = csv_dict[i]
     metadata = liq_meta.render(**row)
@@ -71,7 +83,8 @@ for i in range(0,len(csv_dict)):
       if 'error' in data and 'code' in data['error']:
         print("Upload Error",data['error']['code'],data['error']['info'])
 
-        if data['error']['code']=="ratelimited":   #stop trying and wait. else continue to next
+        if data['error']['code']=="ratelimited":   #stop trying and quit on this error
+          print(datetime.now().strftime("%H:%M:%S"))
           sys.exit()
 
       elif 'upload' in data and 'result' in data['upload']: 
@@ -90,4 +103,4 @@ for i in range(0,len(csv_dict)):
     print("OS Error: Skipping line",i+2,err)
     pass
 
-
+print(datetime.now().strftime("%H:%M:%S"))
