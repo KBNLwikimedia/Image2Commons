16eaf7cacd991b31b7a4c1d7639fc9eb990d8d5d
Rick Companje <rick@companje.nl>
2020-07-07 13:58:12 +0200

    Update GetAllFilesInCategory.py

--- scripts/GetAllFilesInCategory.py
+++ scripts/GetAllFilesInCategory.py
@@ -1,22 +1,8 @@
 #!/usr/local/bin/python3
-import requests,json,re
+# import requests,json,re
 
 # category = 'Images from Het Utrechts Archief'
-category = 'Deelcollectie Nederlandse-Spoorwegen'
-cmcontinue = ''
+from CommonsUpload import getAllFilesInCategory 
 
-while True: 
-  base_url = 'https://commons.wikimedia.org/w/api.php?action=query&list=categorymembers&'\
-            'cmtype=file&cmtitle=Category:'+category+'&'\
-            'cmlimit=max&format=json&cmcontinue=' + cmcontinue
+print(getAllFilesInCategory('Deelcollectie Nederlandse-Spoorwegen'))
 
-  response = requests.get(base_url + cmcontinue)
-  data = response.json()
-
-  for i in data['query']['categorymembers']:
-    print(i['title'])
-
-  if 'continue' in data:
-    cmcontinue = data['continue']['cmcontinue']
-  else: 
-    break
